f27352ada810562d7e69dc0003cc6b4bac9cff35
kuru-bana <kuru-api@hotmail.com>
2026-05-20 05:50:55 +0900

    Create config.ts

---
+++ app/src/config.ts
@@ -0,0 +1,112 @@
+export const YOUTUBE_API_KEYS = [
+  "AIzaSyBQ-40ld7erVfx7s6iKBYl-GjDqJVYBwrc",
+  "AIzaSyCoz9NrmBu5mFRm_-qD4XoTFaqu7AGvGeU",
+  "AIzaSyDdgsY60mxo98j99leqp1pb5aFYvSSvrSc",
+  "AIzaSyC__tVvRkEHBtGIjfxhD_FbG3fAcjiaXlc",
+  "AIzaSyAZwLva1HxzDbKFJuE9RVcxS5B4q_ol8yE",
+  "AIzaSyCqvGnAlX4_Ss7PInUEg3RWucbdjmnWP6U",
+  "AIzaSyBw0JamBkR5eOJLYnmBBxEoptlVm22Q0oA",
+  "AIzaSyCz7f0X_giaGyC9u1EfGZPBuAC9nXiL5Mo",
+  "AIzaSyBmzCw7-sX1vm-uL_u2Qy3LuVZuxye4Wys",
+  "AIzaSyBWScla0K91jUL6qQErctN9N2b3j9ds7HI",
+  "AIzaSyA17CdOQtQRC3DQe7rgIzFwTUjwAy_3CAc",
+  "AIzaSyDdk_yY0tN4gKsm4uyMYrIlv1RwXIYXrnw",
+  "AIzaSyDeU5zpcth2OgXDfToyc7-QnSJsDc41UGk",
+  "AIzaSyClu2V_22XpCG2GTe1euD35_Mh5bn4eTjA",
+]
+
+export const INVIDIOUS_INSTANCES = [
+  "https://invidious.darkness.services",
+  "https://invidious.lol",
+  "https://invidious.st",
+  "https://inv.in.projectsegfau.lt",
+  "https://invidious.twigotech.eu",
+  "https://invidious.mint.lgbt",
+  "https://invidious.einfachzocken.eu",
+  "https://invidious.sethforprivacy.com",
+  "https://invidious.reallyaweso.me",
+  "https://invidious.asir.dev",
+  "https://inv.nadeko.net",
+  "https://yewtu.be",
+  "https://invidious.nerdvpn.de",
+  "https://inv.riverside.rocks",
+]
+
+export const INVIDIOUS_STREAM_INSTANCES = [
+  "https://app.materialio.us",
+  "https://inv.kamuridesu.com",
+  "https://inv.nadeko.net",
+  "https://inv.vern.cc",
+  "https://inv1.nadeko.net",
+  "https://inv2.nadeko.net",
+  "https://inv3.nadeko.net",
+  "https://inv4.nadeko.net",
+  "https://inv5.nadeko.net",
+  "https://inv6.nadeko.net",
+  "https://inv7.nadeko.net",
+  "https://inv8.nadeko.net",
+  "https://inv9.nadeko.net",
+  "https://invidious.f5.si",
+  "https://invidious.lunivers.trade",
+  "https://invidious.nerdvpn.de",
+  "https://invidious.nietzospannend.nl",
+  "https://invidious.projektegfau.lt",
+  "https://invidious.protokolla.fi",
+  "https://invidious.tiekoetter.com",
+  "https://lekker.gay",
+  "https://nyc1.iv.ggtyler.dev",
+  "https://rust.oskamp.nl",
+  "https://y.com.sb",
+  "https://yewtu.be",
+  "https://yt.thechangebook.org",
+  "https://yt.vern.cc",
+  "https://invidious.darkness.services",
+  "https://invidious.privacyredirect.com",
+  "https://invidious.fdn.fr",
+  "https://iv.ggtyler.dev",
+  "https://invidious.lunar.icu",
+  "https://yt.artemislena.eu",
+  "https://invidious.flokinet.to",
+  "https://invidious.sethforprivacy.com",
+  "https://yt.omada.cafe",
+  "https://invidious.ritoge.com",
+]
+
+export const XEROX_API_LIST_URL = "https://raw.githubusercontent.com/choco-1515/About-youtube/refs/heads/main/stream/xerox-api.json"
+export const MIN2_TUBE_API_LIST_URL = "https://raw.githubusercontent.com/choco-1515/About-youtube/refs/heads/main/stream/min-tube-api.json"
+export const WISTA_API_LIST_URL = "https://raw.githubusercontent.com/choco-1515/About-youtube/refs/heads/main/stream/wista-api.json"
+
+export const XEROX_API_FALLBACK = [
+  "https://vpdzyc-3000.csb.app",
+  "https://rt5k4d-3000.csb.app",
+  "https://xeroxyt-nt-apiv1-0ydt.onrender.com",
+  "https://xeroxyt-nt-apiv1-m28t.onrender.com",
+  "https://xeroxyt-nt-apiv1-5vsz.onrender.com",
+]
+
+export const MIN2_TUBE_API_FALLBACK = [
+  "https://min-tube2-api.vercel.app",
+  "https://min-tube-api-3.vercel.app",
+  "https://min-tube-api4.vercel.app",
+  "https://min-tube-api5.vercel.app",
+  "https://server-minp.vercel.app",
+]
+
+export const CORS_PROXIES_STATIC = [
+  "https://api.allorigins.win/raw?url=",
+  "https://api.codetabs.com/v1/proxy?quest=",
+  "https://thingproxy.freeboard.io/fetch/",
+  "https://corsproxy.io/?url=",
+  "https://cors.lol/?url=",
+  "https://yacdn.org/proxy/",
+  "https://cors.wtf/?url=",
+  "https://corsproxy.rocks/?url=",
+  "https://cors.hyoo.ru/?url=",
+  "https://cors.miaouf.com/?url=",
+  "https://cors.x2u.in/",
+  "https://cors-proxy.htmldriven.com/?url=",
+]
+
+export const CORS_PROXY_GITHUB_URL = "https://raw.githubusercontent.com/woolisbest/crosproxy-list/refs/heads/main/main.txt"
+export const INV_WORKING_KEY = 'invidious_working_instances'
+export const CORS_PROXY_CACHE_KEY = 'inv_cors_proxies_github'
